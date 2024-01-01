c3e24fded1896bbd45e73a7ffd76398dfcb649b4
Jacob Lee <JLpython@outlook.com>
2024-01-01 00:41:52 -0800

    feat: create players.index.PlayerIndex class

---
+++ sports_reference/baseball/players/index.py
@@ -0,0 +1,27 @@
+"""
+"""
+
+import string
+
+
+class PlayerIndex:
+    """
+    """
+    _address = "https://www.baseball-reference.com/players/{letter}/"
+
+    def __init__(self, letter: str):
+        if len(letter) != 1 or letter not in string.ascii_letters:
+            raise ValueError(letter)
+        self._letter = letter.lower()
+
+    @property
+    def letter(self) -> str:
+        """
+        """
+        return self._letter
+
+    @property
+    def address(self) -> str:
+        """
+        """
+        return self._address.format(letter=self.letter)
