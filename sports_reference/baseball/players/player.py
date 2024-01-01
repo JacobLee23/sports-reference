dfc3096e8c0be3dfa68f2faa7bc754c92f8658c7
Jacob Lee <JLpython@outlook.com>
2024-01-01 01:13:10 -0800

    feat: create players.player.Player.name

--- sports_reference/baseball/players/player.py
+++ sports_reference/baseball/players/player.py
@@ -16,6 +16,9 @@ class Player:
         self._response = requests.get(self.address)
         self._soup = bs4.BeautifulSoup(self._response.text, features="lxml")
 
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(id={self.id}, name={self.name})"
+
     @property
     def id(self) -> str:
         """
@@ -27,3 +30,9 @@ class Player:
         """
         """
         return self._address.format(letter=self.id[0], id=self.id)
+    
+    @property
+    def name(self) -> str:
+        """
+        """
+        return self._soup.select_one("div#meta > div > h1 > span").text
